9212524ce8b874a39f567744bb6d718856680122
pranalipawar24 <pawarpranali2412@gmail.com>
2026-09-23 19:59:10 +0530

    Fix interview report schema

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -46,7 +46,7 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
     contents: prompt,
     config: {
         responseMimeType: "application/json",
-        responseSchema: zodToJsonSchema(resumePdfSchema),
+        responseSchema: zodToJsonSchema(interviewReportSchema),
     }
 })
 
